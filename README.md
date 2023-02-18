b2b095feaf942132774bb8eb3f7553b67083a65d
Koray Dundar <koray.dundar@exove.com>
2023-02-19 00:13:01 +0200

    Setting up mustache engine and Bootstrap

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # NodeJS__Project__News-App
-# technologies: node.js, bootstrap, express.js,
+# technologies: node.js, bootstrap, express.js, postgres, postico, bootstrap
 # npm packages: express, body-parser, mustache-express, pg-promise
--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
+const mustacheExpress = require('mustache-express');
 const PORT = 3000
 
+// configuring your view engine
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+
+app.get('/register',(reg,res) => {
+    res.render('register')
+})
 
 
 
