30270e46d6fda9a76d6e2a1ca1a3779b42faec7f
Koray Dundar <koray.dundar@exove.com>
2023-02-19 00:49:44 +0200

    implementing user registration

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
-const mustacheExpress = require('mustache-express');
+const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
 const PORT = 3000
+
+
 
 // configuring your view engine
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/register',(req,res) => {
+    let username = req.body.username
+    let password = req.body.password
+
+    console.log(username)
+    console.log(password)
+
+    res.send('REGISTER')
+})
 
 app.get('/register',(reg,res) => {
     res.render('register')
