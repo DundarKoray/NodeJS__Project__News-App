63b39dac08170f4b45f69d8be7f9b4257de8760f
Koray Dundar <koray.dundar@exove.com>
2023-02-20 10:59:59 +0200

    Encrypting Password Using Bcrypt

--- app.js
+++ app.js
@@ -3,42 +3,53 @@ const app = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const pgp = require('pg-promise')()
+const bcrypt = require('bcrypt')
+
 const PORT = 3000
-const CONNECTION_STRING = "postgress://localhost:5432/newsdb"
+const CONNECTION_STRING = "postgres://localhost:5432/newsdb"
+const SALT_ROUNDS = 10
 
 // configuring your view engine
-app.engine('mustache', mustacheExpress())
-app.set('views', './views')
-app.set('view engine', 'mustache')
+app.engine('mustache',mustacheExpress())
+app.set('views','./views')
+app.set('view engine','mustache')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
 const db = pgp(CONNECTION_STRING)
 
 app.post('/register',(req,res) => {
-    let username = req.body.username
-    let password = req.body.password
 
-    db.oneOrNone('SELECT userid FROM users WHERE username = $1',[username])
-    .then((user) => {
-        if(user) {
-           res.render('register',{message: "User name already exist!"})
-        } else {
-            ///insert user into the users table
-            db.none('INSERT INTO users(username, password) VALUES($1,$2)', [username,password])
-            .then(() => {
-                res.send('SUCCESS')
-            })
+  let username = req.body.username
+  let password = req.body.password
+
+  db.oneOrNone('SELECT userid FROM users WHERE username = $1',[username])
+  .then((user) => {
+    if(user) {
+      res.render('register',{message: "User name already exists!"})
+    } else {
+      // insert user into the users table
+
+      bcrypt.hash(password,SALT_ROUNDS,function(error, hash){
+
+        if(error == null) {
+          db.none('INSERT INTO users(username,password) VALUES($1,$2)',[username,hash])
+          .then(() => {
+            res.send('SUCCESS')
+          })
         }
-    })
+
+      })
+
+    }
+  })
+
 })
 
-app.get('/register',(reg,res) => {
-    res.render('register')
+app.get('/register',(req,res) => {
+  res.render('register')
 })
 
-
-
-app.listen(PORT, () => {
-    console.log(`Server has started on ${PORT}`)
+app.listen(PORT,() => {
+  console.log(`Server has started on ${PORT}`)
 })
