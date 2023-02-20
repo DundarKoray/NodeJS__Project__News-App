202891fd951ed8e589a16fb5ffda49678a008d3a
Koray Dundar <koray.dundar@exove.com>
2023-02-20 11:37:01 +0200

    adding session to the news app

--- app.js
+++ app.js
@@ -18,37 +18,62 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 const db = pgp(CONNECTION_STRING)
 
-app.post('/register',(req,res) => {
+// LOGIN PAGE STARTS
+app.get('/login',(req,res) => {
+    res.render('login')
+})
 
-  let username = req.body.username
-  let password = req.body.password
+app.post('/login',(req,res) => {
+    //req.body.username and req.body.password is comming from html name element in forms
+    let username = req.body.username
+    let password = req.body.password
 
-  db.oneOrNone('SELECT userid FROM users WHERE username = $1',[username])
-  .then((user) => {
-    if(user) {
-      res.render('register',{message: "User name already exists!"})
-    } else {
-      // insert user into the users table
-
-      bcrypt.hash(password,SALT_ROUNDS,function(error, hash){
-
-        if(error == null) {
-          db.none('INSERT INTO users(username,password) VALUES($1,$2)',[username,hash])
-          .then(() => {
-            res.send('SUCCESS')
-          })
+    db.oneOrNone('SELECT userid,username,password FROM users WHERE username = $1', [username])
+    .then((user) => {
+        if(user) {
+            // check for user's password
+            // if user exists, we check the the password is matching
+            bcrypt.compare(password, user.password, function(error,result){
+                if(result) {
+                    res.send('Successs!')
+                }
+            })
+        } else {
+            // user does not exist
+            res.render('login', {message: 'This user does not exist! Please register first.'})
         }
-
-      })
-
-    }
-  })
-
+    })
 })
+// LOGIN PAGE ENDS
 
+// REGISTER PAGE STARTS
 app.get('/register',(req,res) => {
-  res.render('register')
+    res.render('register')
 })
+
+app.post('/register',(req,res) => {
+    //req.body.username and req.body.password is comming from html name element in forms
+    let username = req.body.username
+    let password = req.body.password
+
+    db.oneOrNone('SELECT userid FROM users WHERE username = $1',[username])
+    .then((user) => {
+        if(user) {
+            res.render('register',{message: "User name already exists!"})
+        } else {
+            // insert user into the users table
+            bcrypt.hash(password,SALT_ROUNDS,function(error, hash){
+                if(error == null) {
+                    db.none('INSERT INTO users(username,password) VALUES($1,$2)',[username,hash])
+                    .then(() => {
+                    res.send('SUCCESS')
+                    })
+                }
+            })
+        }
+    })
+})
+// REGISTER PAGE ENDS
 
 app.listen(PORT,() => {
   console.log(`Server has started on ${PORT}`)
