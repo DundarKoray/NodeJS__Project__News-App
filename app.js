fca4753e8999e92c4a81724fa78ea2d38f11f25a
Koray Dundar <koray.dundar@exove.com>
2023-02-21 13:31:25 +0200

    implementing express routes and using middleware

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const pgp = require('pg-promise')()
-const bcrypt = require('bcrypt')
 const session = require('express-session')
 const path = require('path')
+const checkAuthorization = require('./utils/checkAuthorization')
+
+const userRoutes = require('./routes/users')
+const indexRoutes = require('./routes/index')
 
 const PORT = 3000
 const CONNECTION_STRING = "postgres://localhost:5432/newsdb"
-const SALT_ROUNDS = 10
 
 const VIEWS_PATH = path.join(__dirname,'/views')
 
@@ -27,159 +29,11 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-const db = pgp(CONNECTION_STRING)
+db = pgp(CONNECTION_STRING)
 
-
-
-
-// ADD ARTICLES PAGE STARTS
-app.get('/users/add-article',(req,res) => {
-    res.render('add-article')
-})
-
-app.post('/users/add-article',(req, res) => {
-    let title = req.body.title
-    let description = req.body.description
-    let userId = req.session.user.userId
-
-    db.none('INSERT INTO articles(title,body,userid) VALUES($1,$2,$3)', [title,description,userId])
-    .then(() => {
-        res.send("SUCCESS")
-    })
-})
-// ADD ARTICLES PAGE ENDS
-
-
-
-
-
-
-// ARTICLES PAGE STARTS
-app.get('/users/articles', (req,res) => {
-
-    // let userId = req.session.user.userId;
-    let userId = 7;
-
-    db.any('SELECT articleid,title,body FROM articles WHERE userid = $1',[userId])
-    .then((articles) => {
-        res.render('articles',{articles: articles})
-    })
-})
-// ARTICLES PAGE ENDS
-
-
-// ARTICLE UPDATE PAGE
-app.post('/users/update-article',(req,res) => {
-
-    let title = req.body.title
-    let description = req.body.description
-    let articleId = req.body.articleId
-
-    db.none('UPDATE articles SET title = $1, body = $2 WHERE articleid = $3', [title,description,articleId])
-    .then(() => {
-        res.redirect('/users/articles')
-    })
-})
-
-
-// ARTICLE EDIT PAGE STARTS
-app.get('/users/articles/edit/:articleId', (req,res) => {
-    let articleId = req.params.articleId
-
-    db.one('SELECT articleid, title, body FROM articles WHERE articleid = $1', [articleId])
-    .then((article) => {
-        res.render('edit-article', article)
-    })
-})
-// ARTICLE EDIT PAGE ENDS
-
-
-
-
-// LOGIN PAGE STARTS
-app.get('/login',(req,res) => {
-    res.render('login')
-})
-
-app.post('/login',(req,res) => {
-    //req.body.username and req.body.password is comming from html name element in forms
-    let username = req.body.username
-    let password = req.body.password
-
-    db.oneOrNone('SELECT userid,username,password FROM users WHERE username = $1', [username])
-    .then((user) => {
-        if(user) {
-            // check for user's password
-            // if user exists, we check the the password is matching
-            bcrypt.compare(password, user.password, function(error,result){
-                if(result) {
-                    // put username and userId in the session
-                    if(req.session) {
-                        req.session.user = {userId: user.userid, username: user.username}
-                    }
-
-                    res.redirect('/users/articles')
-                }
-            })
-        } else {
-            // user does not exist
-            res.render('login', {message: 'This user does not exist! Please register first.'})
-        }
-    })
-})
-// LOGIN PAGE ENDS
-
-
-// Show all articles
-app.get('/',(req,res) => {
-    db.any('SELECT articleid, title,body FROM articles')
-    .then((articles) => {
-        res.render('index',{articles: articles})
-    })
-})
-
-
-// DELETE ARTICLE
-app.post('/users/delete-article',(req,res) => {
-    let articleId = req.body.articleId
-
-    db.none('DELETE FROM articles WHERE articleid = $1',[articleId])
-    .then(() => {
-        res.redirect('/users/articles')
-    })
-})
-
-
-
-
-// REGISTER PAGE STARTS
-app.get('/register',(req,res) => {
-    res.render('register')
-})
-
-app.post('/register',(req,res) => {
-    //req.body.username and req.body.password is comming from html name element in forms
-    let username = req.body.username
-    let password = req.body.password
-
-    db.oneOrNone('SELECT userid FROM users WHERE username = $1',[username])
-    .then((user) => {
-        if(user) {
-            res.render('register',{message: "User name already exists!"})
-        } else {
-            // insert user into the users table
-            bcrypt.hash(password,SALT_ROUNDS,function(error, hash){
-                if(error == null) {
-                    db.none('INSERT INTO users(username,password) VALUES($1,$2)',[username,hash])
-                    .then(() => {
-                    res.send('SUCCESS')
-                    })
-                }
-            })
-        }
-    })
-})
-// REGISTER PAGE ENDS
+// setup routes
+app.use('/',indexRoutes)
+app.use('/users',checkAuthorization,userRoutes)
 
 app.listen(PORT,() => {
   console.log(`Server has started on ${PORT}`)
---
+++ routes/index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const router = express.Router()
+
+const SALT_ROUNDS = 10
+
+// GET
+
+// Show articles page
+router.get('/',(req,res) => {
+    db.any('SELECT articleid, title,body FROM articles')
+    .then((articles) => {
+        res.render('index',{articles: articles})
+    })
+})
+
+// Show register page
+router.get('/register',(req,res) => {
+    res.render('register')
+})
+
+// Show login page
+router.get('/login',(req,res) => {
+    res.render('login')
+})
+
+
+
+
+
+// POST
+
+// Register
+router.post('/register',(req,res) => {
+    //req.body.username and req.body.password is comming from html name element in forms
+    let username = req.body.username
+    let password = req.body.password
+
+    db.oneOrNone('SELECT userid FROM users WHERE username = $1',[username])
+    .then((user) => {
+        if(user) {
+            res.render('register',{message: "User name already exists!"})
+        } else {
+            // insert user into the users table
+            bcrypt.hash(password,SALT_ROUNDS,function(error, hash){
+                if(error == null) {
+                    db.none('INSERT INTO users(username,password) VALUES($1,$2)',[username,hash])
+                    .then(() => {
+                    res.send('SUCCESS')
+                    })
+                }
+            })
+        }
+    })
+})
+
+// Login
+router.post('/login',(req,res) => {
+    //req.body.username and req.body.password is comming from html name element in forms
+    let username = req.body.username
+    let password = req.body.password
+
+    db.oneOrNone('SELECT userid,username,password FROM users WHERE username = $1',[username])
+    .then((user) => {
+        if(user) {
+            // check for user's password
+            // if user exists, we check the the password is matching
+            bcrypt.compare(password, user.password, function(error,result){
+                if(result) {
+                    // put username and userId in the session
+                    if(req.session) {
+                        req.session.user = {userId: user.userid, username: user.username}
+                    }
+
+                    res.redirect('/users/articles')
+                }
+            })
+        } else {
+            // user does not exist
+            res.render('login', {message: 'This user does not exist! Please register first.'})
+        }
+    })
+})
+
+
+module.exports = router
